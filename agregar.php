6e7788642abc377ad4712c06d3da0e73492b14b8
Fr4nAc323 <124444885+Fr4nAc323@users.noreply.github.com>
2023-04-22 15:48:27 -0600

    agregar.php

--- agregar.php
+++ agregar.php
@@ -3,6 +3,4 @@ require_once "CarritoController.php";
 
 $carrito = new CarritoController();
 $carrito->agregarArticulo($_POST['nombre'], $_POST['cantidad'], $_POST['precio_unitario']);
-header("Location: index.php");
-exit();
-?>
+
